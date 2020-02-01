aca65c8c09c1ce09c17ca92fb19ed3a18f23c6d6
Franco Correa <anibalfranciscocorrea@gmail.com>
2020-02-01 17:14:19 -0300

    Form Register

--- src/componets/Auth/Register.js
+++ src/componets/Auth/Register.js
@@ -1,25 +1,66 @@
 import React, { Component } from "react";
-import { Grid, Form, Segment, Button, Header, Icon } from "semantic-ui-react";
+import { Grid, Form, Segment, Button, Header, Icon, Message } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 
 class Register extends Component {
+  state = {};
+
+  handleChange = () => {};
   render() {
     return (
-        <Grid textAlign="center" verticalAlign="middle">
-            <Grid.Column style={{maxWidth: 450}}>
-                <Header as="h2" icon color="orange" text="center">
-                    <Icon name="puzzle piece" color="orange" />
-                    Register for DevChat
-                </Header>
-                <Form size="large">
-                    <Segment stacked>
-                        <Form.Input fluid name="username" icon="user" iconPosition="left" placeholder="Username" type="text"/>
-                        <Form.Input fluid name="username" icon="user" iconPosition="left" placeholder="Username" type="text"/>
-                        <Form.Input fluid name="username" icon="user" iconPosition="left" placeholder="Username" type="text"/>
-                    </Segment>
-                </Form>
-            </Grid.Column>
-        </Grid>    
-    )
+      <Grid textAlign="center" verticalAlign="middle" className="app">
+        <Grid.Column style={{ maxWidth: 450 }}>
+          <Header as="h2" icon color="orange" text="center">
+            <Icon name="puzzle piece" color="orange" />
+            Register for DevChat
+          </Header>
+          <Form size="large">
+            <Segment stacked>
+              <Form.Input
+                fluid
+                name="username"
+                icon="user"
+                iconPosition="left"
+                placeholder="Username"
+                onChange={this.handleChange}
+                type="text"
+              />
+              <Form.Input
+                fluid
+                name="email"
+                icon="mail"
+                iconPosition="left"
+                placeholder="Email Address"
+                onChange={this.handleChange}
+                type="text"
+              />
+              <Form.Input
+                fluid
+                name="password"
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                onChange={this.handleChange}
+                type="password"
+              />
+              <Form.Input
+                fluid
+                name="repeat_password"
+                icon="repeat"
+                iconPosition="left"
+                placeholder="Repeat Password"
+                onChange={this.handleChange}
+                type="password"
+              />
+              <Button color="orange" fluid size="large">
+                Submit
+              </Button>
+            </Segment>
+          </Form>
+          <Message>Already User?<Link to="/login">Login</Link></Message>
+        </Grid.Column>
+      </Grid>
+    );
   }
 }
 
