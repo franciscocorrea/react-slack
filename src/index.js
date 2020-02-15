a3fff02c5779c28c21f22dbb6cecaaa541d0182d
Franco Correa <anibalfranciscocorrea@gmail.com>
2020-02-15 10:35:32 -0300

    Redirect when user is authenticated

--- src/index.js
+++ src/index.js
@@ -1,23 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './componets/App';
-import Login from './componets/Auth/Login';
-import Register from './componets/Auth/Register';
-import registerServiceWorker from './registerServiceWorker';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import App from "./componets/App";
+import Login from "./componets/Auth/Login";
+import Register from "./componets/Auth/Register";
+import registerServiceWorker from "./registerServiceWorker";
+import firebase from "./firebase";
 
-import 'semantic-ui-css/semantic.min.css'
+import "semantic-ui-css/semantic.min.css";
 
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter
+} from "react-router-dom";
 
-const Root = () => (
-    <Router>
-        <Switch>
-            <Route exact path="/" component={App}/>
-            <Route path="/login" component={Login} />
-            <Route path="/register" component={Register} />
-        </Switch>
-    </Router>
-)
+class Root extends Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.history.push("/");
+      }
+    });
+  }
+  render() {
+    return (
+      <Switch>
+        <Route exact path="/" component={App} />
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Switch>
+    );
+  }
+}
 
-ReactDOM.render(<Root />, document.getElementById('root'));
+const RootWithAuth = withRouter(Root);
+
+ReactDOM.render(
+  <Router>
+    <RootWithAuth />
+  </Router>,
+  document.getElementById("root")
+);
 registerServiceWorker();
