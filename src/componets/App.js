71f4312e9e211765d8ee75cd5a09e6aef8f85fc3
Franco Correa <anibalfranciscocorrea@gmail.com>
2020-02-17 16:02:00 -0300

    Pass user for redux to sidepanel

--- src/componets/App.js
+++ src/componets/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from "react";
 import { Grid } from "semantic-ui-react";
 import "./App.css";
+import { connect } from "react-redux";
 
 import ColorPanel from "./ColorPanel/ColorPanel";
 import SidePanel from "./SidePanel/SidePanel";
 import Messages from "./Messages/Message";
 import MetaPanel from "./MetaPanel/MetaPanel";
 
-const App = () => (
+const App = ({ currentUser }) => (
   <Grid columns="equal" className="app" style={{ background: "#eee" }}>
     <ColorPanel />
-    <SidePanel />
+    <SidePanel currentUser={currentUser} />
 
     <Grid.Column style={{ marginLeft: 320 }}>
       <Messages />
@@ -19,8 +20,11 @@ const App = () => (
     <Grid.Column width={4}>
       <MetaPanel />
     </Grid.Column>
-
   </Grid>
 );
 
-export default App;
+const mapStateToProps = state => ({
+  currentUser: state.user.currentUser
+});
+
+export default connect(mapStateToProps)(App);
--- src/componets/SidePanel/SidePanel.js
+++ src/componets/SidePanel/SidePanel.js
@@ -1,18 +1,18 @@
 import React, { Component } from "react";
 import { Menu } from "semantic-ui-react";
-import UserPanel from "./UserPanel"
+import UserPanel from "./UserPanel";
 
 class SidePanel extends Component {
   render() {
+    const { currentUser } = this.props;
     return (
-        <Menu
+      <Menu
         size="large"
         inverted
         fixed="left"
         vertical
-        style={{ background: "#4c3c4c", fontSize: "1.2rem" }}
-      >
-        <UserPanel />
+        style={{ background: "#4c3c4c", fontSize: "1.2rem" }}>
+        <UserPanel currentUser={currentUser}/>
       </Menu>
     );
   }
