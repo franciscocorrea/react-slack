eddab6e795c22336a509f780108704923de5eba0
Franco Correa <anibalfranciscocorrea@gmail.com>
2020-02-22 19:07:29 -0300

    Add component messages and sidebar color panel

--- src/componets/Messages/Message.js
+++ src/componets/Messages/Message.js
@@ -1,8 +1,21 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
+import { Segment, Comment } from "semantic-ui-react";
+import MessageHeader from "./MessageHeader";
+import MessageForm from "./MessageForm"
 
 class Message extends Component {
   render() {
-    return <div>Messages</div>;
+    return (
+      <Fragment>
+        <MessageHeader />
+        <Segment>
+          <Comment.Group className="messages">{/* Messages */}</Comment.Group>
+        </Segment>
+
+        <MessageForm />
+
+      </Fragment>
+    );
   }
 }
 
